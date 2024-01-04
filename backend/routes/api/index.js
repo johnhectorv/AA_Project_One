1087ed62f9efeedf8a2ed38fcd2ab160386e0485
John <johnhvalenzuela@gmail.com>
2024-01-04 17:54:57 -0500

    Airbnb API Doc and some code in api/index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,14 +3,43 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 
-// Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.get('/current-user', (req, res) => {
+  if (requireAuth(req)) {
+    res.status(200).json({
+      user: {
+        id: req.user.id,
+        firstName: req.user.lastname,
+        lastname: req.user.lastname,
+        email: req.user.email,
+        username: req.user.username
+      }
+    });
+  } else {
+    res.status(200).json({
+      user: null
+    });
+  }
+});
+
+router.post('/login', (req, res) => {
+  const { credential, password } = req.body;
+
+  if (!credential || !password) {
+    return res.status(400).json({
+      message: 'Bad Request',
+      errors: {
+        credential: 'Email or username is required',
+        password: 'Password is required'
+      }
+    });
+  }
+});
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });

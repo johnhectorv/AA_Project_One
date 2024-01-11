bce67d797aec3575b19e163185ddce054249e5a3
John <johnhvalenzuela@gmail.com>
2024-01-10 19:04:15 -0500

    fixed migration

--- backend/db/migrations/20240108181153-create-review.js
+++ backend/db/migrations/20240108181153-create-review.js
@@ -1,5 +1,10 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Reviews', {
@@ -32,6 +37,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
+    options.tableName = "Reviews";
+    return queryInterface.dropTable(options);
   }
 };
